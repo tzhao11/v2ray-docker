8a2ac024103a32060fd8650310708eab9983e6a2
Tong Zhao <tzhao11@illinois.edu>
2018-09-04 15:37:25 +0800

    bind to $PORT

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,6 @@ sed -i "s/CLIENT_ID/$CLIENT_ID/g" /etc/v2ray/config.json
 sed -i "s/CLIENT_ALTERID/$CLIENT_ALTERID/g" /etc/v2ray/config.json
 sed -i "s#CLIENT_WSPATH#$CLIENT_WSPATH#g" /etc/v2ray/config.json
 sed -i "s#CLIENT_WSPATH#$CLIENT_WSPATH#g" /etc/nginx/conf.d/default.conf
+sed -i "s#PORT#$PORT#g" /etc/nginx/conf.d/default.conf
 v2ray -config=/etc/v2ray/config.json &
 nginx -g 'daemon off;'
